4a07842644961590824b89b21f41fff18c2aab2f
Iskandar Mamadnazarov <iskandarko@gmail.com>
2020-09-28 11:56:41 +0300

    contact form is fixed

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,11 +8,12 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
-      var firstName = name; // For Success/Failure Message
+      let person = new Object();
+      person.name = $("input#name").val();
+      person.email = $("input#email").val();
+      person.phone = $("input#phone").val();
+      person.message = $("textarea#message").val();
+      let firstName = person.name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
         firstName = name.split(' ').slice(0, -1).join(' ');
@@ -21,13 +22,10 @@ $(function() {
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
         url: "https://formspree.io/xjvpkble",
+        crossDomain: true,
+        dataType: 'json',
         type: "POST",
-        data: {
-          name: name,
-          phone: phone,
-          email: email,
-          message: message
-        },
+        data: person,
         cache: false,
         beforeSend: function() {
           $('#success').html("<div class='alert alert-success'>");
@@ -55,7 +53,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server API is not responding. Please try again later!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
