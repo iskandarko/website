5b884746dd19fd1e84cbae41454beed8077578f3
Iskandar Mamadnazarov <iskandarko@gmail.com>
2020-09-28 09:33:22 +0300

    trying to fix CORS header error

--- js/contact_me.js
+++ js/contact_me.js
@@ -29,6 +29,15 @@ $(function() {
           message: message
         },
         cache: false,
+        beforeSend: function() {
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Sending... </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+        },
         success: function() {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
