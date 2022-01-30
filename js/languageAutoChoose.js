06201977b6522b4fabe42725696b7946ba235726
Iskandar Mamadnazarov <iskandarko@gmail.com>
2022-01-30 22:59:36 +0300

    chore: minor update of CV and language preferences

--- js/languageAutoChoose.js
+++ js/languageAutoChoose.js
@@ -1,20 +1,25 @@
-window.onload = init;
+window.onload = init
 
 function init() {
-  userSpeaksRussian() ? switchLanguage("ru") : switchLanguage("en");
+  // TODO: decide on complete removal of RU version
+  switchLanguage('en')
 }
 
 function userSpeaksRussian() {
-    if (navigator.languages && navigator.languages.length) {
-      for (let i = 0; i < navigator.languages.length; i++) {
-        if (navigator.languages[i] === "ru") {
-          return true;
-        }
-      }
-    } else {
-      if (navigator.userLanguage === "ru" || navigator.language === "ru" || navigator.browserLanguage === "ru"){
-        return true;
+  if (navigator.languages && navigator.languages.length) {
+    for (let i = 0; i < navigator.languages.length; i++) {
+      if (navigator.languages[i] === 'ru') {
+        return true
       }
     }
-    return false;
+  } else {
+    if (
+      navigator.userLanguage === 'ru' ||
+      navigator.language === 'ru' ||
+      navigator.browserLanguage === 'ru'
+    ) {
+      return true
+    }
+  }
+  return false
 }
